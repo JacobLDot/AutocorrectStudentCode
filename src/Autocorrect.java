c7d942bc36041635d1fbd4dce529c8116694c4e1
jacobldot <jacob.lowe@menloschool.org>
2026-03-10 09:31:27 -0700

    Commented version.

--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -1,8 +1,6 @@
-import java.beans.PropertyEditorSupport;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
